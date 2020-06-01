c930a17b79c186bab9ea380b850aa1b62cdf5fb5
Kathryn Rhoads <kathryn.ann.rhoads@gmail.com>
2020-06-01 15:43:12 -0400

    added helper function to determine winner

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -33,6 +33,10 @@ function Square(props){
     handleClick(i){
       //call .slice to create a copy of the squares array to modify instead of exsiting array
       const squares = this.state.squares.slice()
+      //ignore a click if someone has won the game or if the square has already been clicked
+      if(calculateWinner(squares)|| squares[i]){
+        return
+      }
       squares[i] = this.state.xIsNext ? 'X': 'O';
       this.setState({
         squares: squares,
@@ -49,8 +53,14 @@ function Square(props){
     }
   
     render() {
-      const status = 'Next player: X';
-  
+      const winner = calculateWinner(this.state.squares)
+      let status;
+      // const status = 'Next player: ' +(this.state.xIsNext ? 'X': 'O');
+      if(winner){
+        status = 'Winner: ' + winner
+      }else{
+        status = 'Next player: ' +(this.state.xIsNext ? 'X': 'O');
+      }
       return (
         <div>
           <div className="status">{status}</div>
@@ -88,6 +98,26 @@ function Square(props){
         </div>
       );
     }
+  }
+  //function to determine winner
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
   
   // ========================================
